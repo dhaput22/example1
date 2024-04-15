cad8bfc48c23227aaa6f2193a75f2928e4afe0ef
Muhammad Dafa Putra <muhammaddavaputra@gmail.com>
2024-04-16 06:37:00 +0700

    List_barang

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +14,31 @@ use App\Http\Controllers\HomeController;
 |
 */
 
- //Route::get('/', function () {
- // return view('welcome');
- // });
+ Route::get('/welcome', function () {
+  return view('welcome');
+  });
+
+ Route::get('/user/{id}', function ($id) {
+  return 'User dengan ID ' . $id;
+  });
+
+ Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+ });
+
+// Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+//    return view('list_barang', compact('id', 'nama'));
+// });
+
+//Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+
+ Route::get('/list_barang', [ListBarangController::class, 'tampilkan']);
 
  Route::get('/', [HomeController::class, 'index']);
  Route::get('/contact', [HomeController::class, 'contact']);
